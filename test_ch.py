d4236098852363d27edcb622bd4b7af6db463aa1
dear上 <ysctrnt@126.com>
2018-02-28 02:18:29 +0800

    try requests module

--- test_ch.py
+++ test_ch.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#mport sys
+import sys,os
+import requests
+import urllib
 #reload(sys)
 #sys.setdefaultencoding('utf-8')
 
 print u'中文测试'
 print 'scottyuan'
 print 'juliehwang'
+
+url = 'http://www.baidu.com'
+
+res = requests.get(url)
+print res.status_code
